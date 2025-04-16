ce01161d6d0c094e49363bd0695ed0f48dbf182c
natthawutGitZ <natthawut.niny@gmail.com>
2025-04-16 19:54:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from datetime import datetime
 from typing import Optional
 from discord.ext import commands
 from discord import app_commands
+from keep_alive import keep_alive
 import pytz
 
 # Logging Configuration
